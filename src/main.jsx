72e8bbae8213ea6a4a2ab29166074ffe4b992ee0
klich84 <klich84@gmail.com>
2023-03-20 11:51:46 -0500

    feat(Curso React y Redux Clase 11): :moyai: Implentando custom middlewares

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Provider } from 'react-redux'
-import { legacy_createStore as createStore } from 'redux'
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux'
 import { pokemonsReducer } from './reducers/pokemons'
+import { featuring, logger } from './middlewares'
 
-const store = createStore(
-  pokemonsReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composeEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, featuring)
 )
+
+const store = createStore(pokemonsReducer, composeEnhancers)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
